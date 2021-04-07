072cd0a48b92576269af559f8bcd5fa502411dfa
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-04-07 22:36:48 +0700

    camera

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { isAvailable, requestCameraPermissions, takePicture } from '@nativescript/camera';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title = 'camera';
 
-  constructor() { }
+  public picture: any;
 
-  ngOnInit() {
+  public constructor() {
+    this.picture = "https://placehold.it/200x200";
+  }
+  ngOnInit(){
+    if (isAvailable()) {
+      requestCameraPermissions()
+        .then(
+          fulfilled => {
+            console.log('requestCameraPermissions fulfilled.');
+          },
+          rejected => {
+            console.log('No camera permissions set.');
+          }
+        )
+    } else {
+      console.log('No camera detected of available.');
+    }
+  }
+
+  capture(): void {
+    var options = { width: 300, height: 300, keepAspectRatio: true, saveToGallery: false };
+
+    takePicture(options)
+      .then(imageAsset => {
+        this.picture = imageAsset;
+      }).catch(function (err) {
+        console.log("Error -> " + err.message);
+      });
   }
 }
